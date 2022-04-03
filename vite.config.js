32c2522c4f862f5c7948bfef7784cad289bcad7f
kurehajime <xiidec@gmail.com>
2022-04-03 23:28:31 +0900

    日付追加

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
+import { createHtmlPlugin } from 'vite-plugin-html'
+const date = require("date-utils");
+const timestamp = new Date().toFormat("YYYYMMDDHH24MI");
 export default {
     base: "./",
     build:{
         outDir:"docs",
         assetsDir:"./"
-    }
+    },
+    plugins: [
+        createHtmlPlugin({
+        pages: [
+            {
+                filename: 'index.html',
+                template: 'index.html',
+                injectOptions: {
+                  data: {
+                    TIMESTAMP: timestamp,
+                  },
+                },
+            }
+        ],
+        }),
+      ],
   }
